d698515295e0f3737db2384024f0c46676f142b1
Nanatsuki <514948303@qq.com>
2019-05-23 17:21:09 +0800

    开始学习typescript

--- type-redux/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import AppRouter from './app/appRouter'
-import Provider from './redux/provider'
-import {store} from './store/createStore'
-
-ReactDOM.render(<Provider store={store}>
-    <AppRouter />
-</Provider>, document.getElementById('root'))
